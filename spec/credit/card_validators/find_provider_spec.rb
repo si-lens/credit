4000ebdcc85e58e3ab718bdbf0f465ea2b42046d
Szymon Soczewka <sz.soczewka@gmail.com>
2022-09-25 19:00:31 +0200

    Add contexts, use described_class keyword

--- spec/credit/card_validators/find_provider_spec.rb
+++ spec/credit/card_validators/find_provider_spec.rb
@@ -5,45 +5,51 @@ require_relative "../../spec_helper"
 
 RSpec.describe Credit::CardValidators::FindProvider do
   describe "#call" do
-    it "Returns AMEX for american express card number" do
-      card_numbers = [378_282_246_310_005, "371449635398431"]
-      card_numbers.each do |card_number|
-        expect(Credit::CardValidators::FindProvider.new(card_number).call)
-          .to eq(Credit::Card::AMERICAN_EXPRESS)
+    context "When calling for american express card number" do
+      it "Returns 'AMEX'" do
+        card_numbers = [378_282_246_310_005, "371449635398431"]
+        card_numbers.each do |card_number|
+          expect(described_class.new(card_number).call)
+            .to eq(Credit::Card::AMERICAN_EXPRESS)
+        end
       end
     end
-
-    it "Returns MASTERCARD for mastercard card number" do
-      card_numbers = [5_555_555_555_554_444, "5555555555554444",
-                      5_105_105_105_105_100, "5105105105105100"]
-      card_numbers.each do |card_number|
-        expect(Credit::CardValidators::FindProvider.new(card_number).call)
-          .to eq(Credit::Card::MASTERCARD)
+    context "When calling for mastercard card number" do
+      it "Returns 'MASTERCARD'" do
+        card_numbers = [5_555_555_555_554_444, "5555555555554444",
+                        5_105_105_105_105_100, "5105105105105100"]
+        card_numbers.each do |card_number|
+          expect(described_class.new(card_number).call)
+            .to eq(Credit::Card::MASTERCARD)
+        end
       end
     end
-
-    it "Returns VISA for visa card number" do
-      card_numbers = [4_111_111_111_111_111, "4111111111111111",
-                      4_012_888_888_881_881, "4012888888881881"]
-      card_numbers.each do |card_number|
-        expect(Credit::CardValidators::FindProvider.new(card_number).call)
-          .to eq(Credit::Card::VISA)
+    context "When calling for visa card number" do
+      it "Returns 'VISA'" do
+        card_numbers = [4_111_111_111_111_111, "4111111111111111",
+                        4_012_888_888_881_881, "4012888888881881"]
+        card_numbers.each do |card_number|
+          expect(described_class.new(card_number).call)
+            .to eq(Credit::Card::VISA)
+        end
       end
     end
-
-    it "Returns INVALID for invalid card number" do
-      card_numbers = [1_111_111_111_111_111, "211111111"]
-      card_numbers.each do |card_number|
-        expect(Credit::CardValidators::FindProvider.new(card_number).call)
-          .to eq(Credit::Card::INVALID)
+    context "When calling for invalid card number" do
+      it "Returns 'INVALID'" do
+        card_numbers = [1_111_111_111_111_111, "211111111"]
+        card_numbers.each do |card_number|
+          expect(described_class.new(card_number).call)
+            .to eq(Credit::Card::INVALID)
+        end
       end
     end
-
-    it "Raises ArgumentError for nonnumerical argument" do
-      card_numbers = ["1_abc_567_890", "", "abc", nil, false, true]
-      card_numbers.each do |_card_number|
-        expect { Credit::CardValidators::FindProvider.new("123das") }
-          .to raise_error(ArgumentError)
+    context "When calling for nonnumerical argument" do
+      it "Raises ArgumentError for nonnumerical argument" do
+        card_numbers = ["1_abc_567_890", "", "abc", nil, false, true]
+        card_numbers.each do |_card_number|
+          expect { described_class.new("123das") }
+            .to raise_error(ArgumentError)
+        end
       end
     end
   end
